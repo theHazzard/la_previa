773c26fd20335fff8a311e14c3fa0316eeae1cdf
Enzo Luis Strongoli <thehazzard@gmail.com>
2012-10-08 23:48:46 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaPrevia::Application.config.secret_token = 'fd01226bee230b7e986c30c1c43da2867420cb00ccc13b4d662cdf7c862480a52437be5f492f13bc8a5f37885478a8b725397146e8ab801ea6e133096bbf1dbc'
